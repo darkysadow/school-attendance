0c876a3f93dc896bd277f85faa9ae4287371e4ed
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-11-22 21:00:14 +0200

    Added styles for active tab in UserSidebar

--- components/UserSidebar/UserSidebar.jsx
+++ components/UserSidebar/UserSidebar.jsx
@@ -1,4 +1,7 @@
+'use client'
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
 
 const UserSidebar = (props) => {
     const links = [
@@ -19,9 +22,12 @@ const UserSidebar = (props) => {
             title: 'Зауваження',
         }
     ]
+    const pathname = usePathname()
+    const slug = pathname.substring(pathname.lastIndexOf("/") + 1);
+
     return (
         <div className="flex flex-col items-start justify-start w-[200px] gap-2 border-blue-500 border-opacity-25 border-r-[0.05px] mr-3">
-            {links.map((link, index) => (<Link key={index} href={`/cabinet/${link.name}`} passHref className="hover:text-blue-900 transition-all">
+            {links.map((link, index) => (<Link key={index} href={`/cabinet/${link.name}`} passHref className={link.name === slug ? "text-blue-900 hover:text-blue-500 transition-all" : "hover:text-blue-900 transition-all"}>
                 {link.title}
             </Link>))}
         </div>

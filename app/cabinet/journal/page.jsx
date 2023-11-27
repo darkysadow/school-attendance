21da4e8381cf3fbb1fec0bda32c5ebd6e477a99c
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-11-27 21:17:40 +0200

    Stopped tracking .env File

--- app/cabinet/journal/page.jsx
+++ app/cabinet/journal/page.jsx
@@ -1,7 +1,39 @@
-export default function UserJournal() {
+import { fetcher } from "@/lib/api"
+import Link from "next/link";
+
+export default async function UserJournal() {
+    //HARDCODE PARAMETERS
+    const classIdentifier = 1
+
+    /******************/
+    const responseData = await fetcher(`${process.env.STRAPI_API_URL}/classes/${classIdentifier}?populate=subjects`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    const subjectsList = responseData.data.attributes.subjects.data;
     return (
-        <div>
-            Журнал
+        <div className="w-full">
+            <h2 className="mx-auto text-center">Оберіть дисципліну з списку:</h2>
+            {/* <ul>
+                {subjectsList.map((subject) => (
+                    <li key={subject.id}>
+                        {subject.attributes.name}
+                    </li>
+                ))}
+            </ul> */}
+            <div className="mx-auto py-2 px-3 flex flex-row justify-between gap-y-8 w-[50%] flex-wrap">
+                {subjectsList.map((subject) => (
+                    <Link 
+                        key={subject.id} 
+                        className="p-5 flex flex-row rounded bg-emerald-100 border border-emerald-200 hover:scale-[1.02] transition-all"
+                        href={`journal/subject`}
+                        passHref
+                    >
+                        {subject.attributes.name}
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
--- app/cabinet/layout.jsx
+++ app/cabinet/layout.jsx
@@ -2,13 +2,13 @@ import UserSidebar from "@/components/UserSidebar/UserSidebar";
 import Link from "next/link";
 
 export default function CabinetLayout({children}) {
-    const userRole = 'Administration'
+    const userRole = 'Parent'
     switch (userRole) {
         case 'Parent':
             return (
-                <main className="container mx-auto min-h-[82vh] flex flex-row my-4 px-5">
+                <main className="container w-full mx-auto min-h-[82vh] flex flex-row my-4 px-5">
                     <UserSidebar />
-                    <div >
+                    <div className="w-full">
                         {children}
                     </div>
                 </main>

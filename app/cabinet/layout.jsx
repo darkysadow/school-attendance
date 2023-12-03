634066c3abbdf2480f03f75609b6eded62efdf93
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-12-03 02:01:09 +0200

    Refactored UserSidbar

--- app/cabinet/layout.jsx
+++ app/cabinet/layout.jsx
@@ -1,45 +1,16 @@
 import UserSidebar from "@/components/UserSidebar/UserSidebar";
 import Link from "next/link";
 
-export default function CabinetLayout({children}) {
-    const userRole = 'Parent'
-    switch (userRole) {
-        case 'Parent':
-            return (
-                <main className="container w-full mx-auto min-h-[82vh] flex flex-row my-4 px-5">
-                    <UserSidebar />
-                    <div className="w-full">
-                        {children}
-                    </div>
-                </main>
-            )
-        case 'Teacher':
-            return (
-                <main className="container mx-auto min-h-[82vh] flex flex-row my-4 px-5">
-                    <ul>
-                        <li>Класи</li>
-                        <li>Дисципліни</li>
-                    </ul>
-                    <div >
-                        {children}
-                    </div>
-                </main>
-            )
-        case 'Administration':
-            return (
-                <main className="container mx-auto min-h-[82vh] flex flex-row my-4 px-5">
-                    <ul>
-                        <li>Класи</li>
-                        <li>Викладачі</li>
-                        <li>Диципліни</li>
-                    </ul>
-                    <div >
-                        {children}
-                    </div>
-                </main>
-            )
-        default:
-            break;
-    }
-    
+export default function CabinetLayout({ children }) {
+    const userRole = 'Administration'
+
+    return (
+        <main className="container w-full mx-auto min-h-[82vh] flex flex-row my-4 px-5">
+            <UserSidebar role={userRole} />
+            <div className="w-full">
+                {children}
+            </div>
+        </main>
+    )
+
 }
--- components/UserSidebar/UserSidebar.jsx
+++ components/UserSidebar/UserSidebar.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
 
-const UserSidebar = (props) => {
-    const links = [
+const UserSidebar = ({ role }) => {
+    const parentLinks = [
         {
             name: 'subjects',
             title: 'Предмети',
@@ -22,16 +22,62 @@ const UserSidebar = (props) => {
             title: 'Зауваження',
         }
     ]
+
+    const teacherLinks = [
+        {
+            name: 'teacherClasses',
+            title: 'Класи',
+        },
+        {
+            name: 'teacherSubjects',
+            title: 'Дисципліни',
+        },
+    ]
+
+    const adminLinks = [
+        {
+            name: 'adminClasses',
+            title: 'Класи',
+        },
+        {
+            name: 'adminTeachers',
+            title: 'Викладачі',
+        },
+        {
+            name: 'adminSubjects',
+            title: 'Дисципліни',
+        },
+    ]
     const pathname = usePathname()
     const slug = pathname.substring(pathname.lastIndexOf("/") + 1);
-
-    return (
-        <div className="flex flex-col items-start justify-start w-[200px] gap-2 border-blue-500 border-opacity-25 border-r-[0.05px] mr-3">
-            {links.map((link, index) => (<Link key={index} href={`/cabinet/${link.name}`} passHref className={link.name === slug ? "text-blue-900 hover:text-blue-500 transition-all" : "hover:text-blue-900 transition-all"}>
-                {link.title}
-            </Link>))}
-        </div>
-    )
+    switch (role) {
+        case 'Parent':
+            return (
+                <div className="flex flex-col items-start justify-start w-[200px] gap-2 border-blue-500 border-opacity-25 border-r-[0.05px] mr-3">
+                    {parentLinks.map((link, index) => (<Link key={index} href={`/cabinet/${link.name}`} passHref className={link.name === slug ? "text-blue-900 hover:text-blue-500 transition-all" : "hover:text-blue-900 transition-all"}>
+                        {link.title}
+                    </Link>))}
+                </div>
+            )
+        case 'Teacher':
+            return (
+                <div className="flex flex-col items-start justify-start w-[200px] gap-2 border-blue-500 border-opacity-25 border-r-[0.05px] mr-3">
+                    {teacherLinks.map((link, index) => (<Link key={index} href={`/cabinet/${link.name}`} passHref className={link.name === slug ? "text-blue-900 hover:text-blue-500 transition-all" : "hover:text-blue-900 transition-all"}>
+                        {link.title}
+                    </Link>))}
+                </div>
+            )
+        case 'Administration':
+            return (
+                <div className="flex flex-col items-start justify-start w-[200px] gap-2 border-blue-500 border-opacity-25 border-r-[0.05px] mr-3">
+                    {adminLinks.map((link, index) => (<Link key={index} href={`/cabinet/${link.name}`} passHref className={link.name === slug ? "text-blue-900 hover:text-blue-500 transition-all" : "hover:text-blue-900 transition-all"}>
+                        {link.title}
+                    </Link>))}
+                </div>
+            )
+        default:
+            break;
+    }
 }
 
 export default UserSidebar;

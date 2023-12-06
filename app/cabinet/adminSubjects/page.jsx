63ea8b96aa69cb52509da370a8fad193dfd0a324
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-12-06 20:22:11 +0200

    Realised adding teachers to subjects feature

--- app/cabinet/adminSubjects/page.jsx
+++ app/cabinet/adminSubjects/page.jsx
@@ -2,12 +2,15 @@ import AdminSubjectsList from "@/components/AdminSubjectsList/AdminSubjectsList"
 import { fetcher } from "@/lib/api"
 
 export default async function AdminSubjects() {
-    const responseData = await fetcher(`${process.env.STRAPI_API_URL}/subjects?populate=teachers`)
-    const subjectsList = responseData.data
+    const subjectsList = await fetcher(`${process.env.STRAPI_API_URL}/subjects?populate=teachers`, {cache: 'no-store'}).then(response => response.data)
+    const teachersList = await fetcher(`${process.env.STRAPI_API_URL}/teachers`, {cache: 'no-store'}).then(response => response.data)
     return(
         <div>
             <h1>Список дисциплін</h1>
-            <AdminSubjectsList subjectsL={subjectsList} />
+            <AdminSubjectsList 
+                subjectsL={subjectsList}
+                teachersL={teachersList}
+            />
         </div>
     )
 }
--- components/AdminSubjectsList/AdminSubjectsList.jsx
+++ components/AdminSubjectsList/AdminSubjectsList.jsx
@@ -1,24 +1,51 @@
 "use client"
 
 import { addNewSubject } from "@/lib/actions/addNewSubject";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Input, InputLabel, TextField } from "@mui/material";
+import addTeacherToSubject from "@/lib/actions/addTeacherToSubject";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Input, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 
-const AdminSubjectsList = ({ subjectsL }) => {
+const AdminSubjectsList = ({ subjectsL, teachersL }) => {
     const [subjectsList, setSubjectsList] = useState(subjectsL)
-    const [isAddDialogOpen, setAddDialogOpen] = useState(false)
+    const [teachersList, setTeachersList] = useState(teachersL)
+    const [isAddSubjectDialogOpen, setAddSubjectDialogOpen] = useState(false)
     const [newSubjectValue, setNewSubjectValue] = useState('')
+    const [isAddTeacherToSubjectDialogOpen, setAddTeacherToSubjectDialogOpen] = useState(false)
+    const [selectedSubject, setSelectedSubject] = useState(undefined)
+    const [teacherSelectedToAdd, setTeacherSelectedToAdd] = useState('')
 
-    const closeAddDialog = () => {
-        setAddDialogOpen(false)
+    const closeAddSubjectDialog = () => {
+        setAddSubjectDialogOpen(false)
         setNewSubjectValue('')
+    }
+
+    const closeAddTeacherToSubjectDialog = () => {
+        setTeachersList(teachersL)
+        setSelectedSubject(undefined)
+        setTeacherSelectedToAdd('')
+        setAddTeacherToSubjectDialogOpen(false)
     }
 
     const handleAddNewSubject = async () => {
         const response = await addNewSubject(newSubjectValue).then(response => response.data)
         setSubjectsList(prevData => [...prevData, response])
-        setAddDialogOpen(false)
+        setAddSubjectDialogOpen(false)
         setNewSubjectValue('')
+    }
+
+    const handleAddTeacherToSubject = (subject) => {
+        setSelectedSubject(subject)
+        setTeachersList(prevTeachers => prevTeachers.filter(teacher => !subject.attributes.teachers.data.some(t => t.id === teacher.id)));
+        setAddTeacherToSubjectDialogOpen(true)
+    }
+
+    const addTeacherToSubjectAdmin = async () => {
+        const responseData = await addTeacherToSubject(teacherSelectedToAdd.id, selectedSubject.id)
+        const updatedSubjectsList = subjectsList.map(subject => {
+            return subject.id === responseData.data.id ? responseData.data : subject;
+        });
+        setSubjectsList(updatedSubjectsList);
+        setAddTeacherToSubjectDialogOpen(false)
     }
 
 
@@ -27,21 +54,25 @@ const AdminSubjectsList = ({ subjectsL }) => {
         <div className="my-4">
             <ul>
                 {subjectsList.map((subject, index) => (
-                    <li key={subject.id}>
+                    <li key={subject.id} className="my-4 border-b-[0.5px] border-sky-300 border-opacity-30 py-2">
                         {index + 1}. {subject.attributes.name} <span className="text-slate-500 font-light">ID: {subject.id}</span><br />
                         {
                             subject.attributes.teachers.data.length === 0 ? 
                             <span className="ml-4 text-red-400">Викладачі не призначені!</span> :
-                            <span className="ml-4 text-red-400">Викладачі: </span>
-                        } <br />
+                            <span className="ml-4 text-green-400">Викладачі: </span>
+                        } <Button onClick={() => handleAddTeacherToSubject(subject)}>Додати викладача +</Button> <br />
                         {subject.attributes.teachers.data.map((teacher) =>
                             
-                            <span key={teacher.id} className="ml-4 text-cyan-500">{teacher.attributes.name} <span className="text-slate-500 font-light">ID: {teacher.id}</span></span>)}
+                            <div key={teacher.id}>
+                                <span className="ml-4 text-cyan-500">{teacher.attributes.name} <span className="text-slate-500 font-light">ID: {teacher.id}</span></span>
+                                <br/>
+                            </div>
+                            )}
                     </li>
                 ))}
-                <Button onClick={() => setAddDialogOpen(true)}>Додати нову дисципліну +</Button>
+                <Button onClick={() => setAddSubjectDialogOpen(true)}>Додати нову дисципліну +</Button>
             </ul>
-            <Dialog open={isAddDialogOpen} onClose={closeAddDialog}>
+            <Dialog open={isAddSubjectDialogOpen} onClose={closeAddSubjectDialog}>
                 <DialogTitle>Додати дисципліну до списку</DialogTitle>
                 <DialogContent>
                     <TextField
@@ -52,9 +83,38 @@ const AdminSubjectsList = ({ subjectsL }) => {
                     />
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={closeAddDialog}>Закрити</Button>
+                    <Button onClick={closeAddSubjectDialog}>Закрити</Button>
                     <Button onClick={handleAddNewSubject} disabled={!newSubjectValue || newSubjectValue.length < 2}>Додати</Button>
                 </DialogActions>
+            </Dialog>
+            <Dialog open={isAddTeacherToSubjectDialogOpen} onClose={closeAddTeacherToSubjectDialog}>
+                <DialogTitle>Додати викладача до предмету</DialogTitle>
+                <DialogContent>
+                    Предмет: <br/>
+                    {selectedSubject?.attributes?.name} <span className="text-slate-500 font-light">ID: {selectedSubject?.id}</span>
+                </DialogContent>
+                <FormControl fullWidth className="my-2">
+                        <InputLabel id="select-teacher-to-subject" className="ml-2">Оберіть викладача</InputLabel>
+                        <Select
+                            className="mx-2"
+                            labelId="select-teacher-to-subject"
+                            value={teacherSelectedToAdd?.id ? teacherSelectedToAdd.id : ''}
+                            onChange={(e) => setTeacherSelectedToAdd(teachersList.find(item => item.id === e.target.value))}
+                            label="Оберіть викладача"
+                        
+                        >
+                            <MenuItem value={undefined}>
+                                <em>Оберіть Викладача</em>
+                            </MenuItem>
+                            {teachersList && teachersList.map((teacher) => (
+                                <MenuItem key={teacher.id} value={teacher.id}>{teacher.attributes.name} <span className="text-slate-500 font-light"> ID: {teacher.id}</span></MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <DialogActions>
+                        <Button onClick={closeAddTeacherToSubjectDialog}>Закрити</Button>
+                        <Button onClick={addTeacherToSubjectAdmin} disabled={teacherSelectedToAdd === '' || teacherSelectedToAdd === undefined}>Додати</Button>
+                    </DialogActions>
             </Dialog>
         </div>
     )

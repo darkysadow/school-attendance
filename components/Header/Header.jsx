562bcc4b36ae20f0a819d014c05d2c24e611b3f4
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-12-07 19:41:21 +0200

    Added authorization

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,21 +1,93 @@
+"use client"
 
-import { Button, TextField } from "@mui/material"
+
+import { tryLogin } from "@/lib/actions/authorisation"
+import { setToken, unsetToken } from "@/lib/auth"
+import { UserProvider, useFetchUser, useUser } from "@/lib/authProvider"
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material"
+import Link from "next/link"
+import { useState } from "react"
 
 const Header = (props) => {
+    const {user, loading} = useFetchUser()
+    const [formData, setFormData] = useState({login: '', password: ''})
+    const [isErrorDialogOpen, setErrorDialogOpen] = useState(false)
+    const [error, setError] = useState(undefined)
 
-    return(
-        <header className="px-5">
-            <div className="container mx-auto px-5 py-4 flex flex-row justify-between items-center border-blue-500 border-opacity-25 border-b-[0.05px]">
-                <div className="font-pacifico tracking-widest">
-                    <span className="text-red-600">є</span>Журнал
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const loginError = (error) => {
+        setError(error)
+        setErrorDialogOpen(true)
+    }
+
+    const closeErrorDialog = () => {
+        setErrorDialogOpen(false)
+        setError(undefined)
+        setFormData({login: '', password: ''})
+    }
+
+    const handleSubmit = async () => {
+        const responseData = await tryLogin(formData)
+        if(responseData.data === null) {
+            loginError(responseData.error)
+        } else {
+            const isTokenSetted = setToken(responseData)
+            if(isTokenSetted === true) {
+                location.reload()
+                
+            }
+        }
+    }
+
+    const handleLogout = () => {
+        const logoutResult = unsetToken()
+        if(logoutResult) {
+            router.push('/')
+        }
+    }
+
+    return (
+        <UserProvider value={{user, loading}}>
+            <header className="px-5">
+                <div className="container mx-auto px-5 py-4 flex flex-row justify-between items-center border-blue-500 border-opacity-25 border-b-[0.05px]">
+                    <div className="font-pacifico tracking-widest">
+                        <span className="text-red-600">є</span>Журнал
+                    </div>
+                    <div className="flex flex-row gap-3">
+                    {!loading && (user ? (
+                    <li className="list-none"><Link href={'/cabinet'} className="hover:text-sky-400 transition-all">Кабінет</Link></li>
+                ) : (''))}
+                {!loading && (user ? (
+                    <li onClick={() => handleLogout()} className="hover:cursor-pointer list-none hover:text-red-400 transition-all">Logout</li>
+                ) : (''))}
+                {!loading && !user ? (
+                    <>
+                        <TextField size="small" label='Логін' variant="filled" name="login" value={formData.login} onChange={handleChange}/>
+                        <TextField type="password" size="small" label="Пароль" name="password" variant="filled" value={formData.password} onChange={handleChange} />
+                        <Button variant="outlined" onClick={handleSubmit}>Увійти</Button>
+                    </>
+                ) : ""}
+                        
+                    </div>
                 </div>
-                <div className="flex flex-row gap-3">
-                <TextField size="small" label='Логін' variant="filled" />
-                <TextField type="password" size="small" label="Пароль" variant="filled"/>
-                <Button variant="outlined">Увійти</Button>
-                </div>
-            </div>
-        </header>
+            </header>
+            <Dialog open={isErrorDialogOpen} onClose={closeErrorDialog}>
+                <DialogTitle>Помилка авторизації</DialogTitle>
+                <DialogContent>
+                    Код помилки: {error?.status}<br/>
+                    Повідомлення <p className="text-red-600">{error?.message}</p><br />
+                    Введіть коректні данні!
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeErrorDialog}>
+                        Закрити
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </UserProvider>
     )
 }
 

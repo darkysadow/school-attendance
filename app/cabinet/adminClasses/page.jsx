d998424b0ee2f45cb6ed3073816a2955679bd01c
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-12-08 03:43:00 +0200

    Added auth checking to adminClasses page

--- app/cabinet/adminClasses/page.jsx
+++ app/cabinet/adminClasses/page.jsx
@@ -1,4 +1,85 @@
-import { fetcher } from "@/lib/api"
+"use client"
+
+import Preloader from "@/components/Preloader/Preloader";
+import getClasses from "@/lib/actions/admin/getClasses";
+import { authMe } from "@/lib/actions/authorisation";
+import { getTokenFromLocalCookie } from "@/lib/auth";
+import { useFetchUser } from "@/lib/authProvider";
+import { getNumberOfClass } from "@/lib/classNumber";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function AdminClasses() {
+    const { user, loading } = useFetchUser();
+    const jwt = getTokenFromLocalCookie();
+    const [role, setRole] = useState(undefined);
+    const [isRoleLoading, setIsRoleLoading] = useState(true);
+    const [classes, setClasses] = useState(undefined);
+    const [isClassesLoading, setIsClassesLoading] = useState(undefined);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (user && !loading) {
+                if (!role) {
+                    const response = await authMe(jwt);
+                    setRole(response.role.name);
+                    setIsRoleLoading(false)
+                } else {
+                    if (role === "Administrator") {
+                        if (classes === undefined) {
+                            const serverClassesData = await getClasses();
+                            serverClassesData?.data ? setClasses(serverClassesData.data) : setClasses(undefined);
+                            setIsClassesLoading(false)
+                        }
+                    }
+                }
+            }
+        };
+
+        fetchData();
+    }, [user, role, loading, jwt, classes]);
+
+    if (user && !loading) {
+        if (role === "Administrator" && !isRoleLoading) {
+            if (classes?.length === 0 && !isClassesLoading) {
+                return (
+                    <div>Список класів пустий!</div>
+                )
+            } else if (!isClassesLoading) {
+                return (
+                    <div className="w-full">
+                        <h2 className="mx-auto text-center">Оберіть навчальний клас:</h2>
+                        <div className="mx-auto py-2 px-3 flex flex-row justify-between gap-y-8 w-[50%] flex-wrap">
+                            {classes && classes.map((item) => (
+                                <Link 
+                                    key={item.id} 
+                                    className="p-5 flex flex-row rounded bg-emerald-100 border border-emerald-200 hover:scale-[1.02] transition-all"
+                                    href={`adminClasses/${item.id}`}
+                                    passHref
+                                >
+                                    {getNumberOfClass(item.attributes.admissionYear)}-{item.attributes.classLetter}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                )
+            } else {
+                return <Preloader />
+            }
+
+        } else if (role !== "Administrator" && !isRoleLoading) {
+            return <div>Ви не авторизовані в профілі вчителя!</div>;
+        } else {
+            return <Preloader />
+        }
+    } else if (!user && !loading) {
+        return <div className="w-full text-center">Ви не можете переглянути сторінку, оскільки не авторизувались!</div>;
+    } else {
+        return <Preloader />
+    }
+}
+
+/* import { fetcher } from "@/lib/api"
 import { getNumberOfClass } from "@/lib/classNumber"
 import Link from "next/link"
 
@@ -22,4 +103,4 @@ export default async function AdminClasses() {
             </div>
         </div>
     )
-}
+} */
